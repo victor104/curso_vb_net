3070bd17422ca350c77403b1662b3e468fde97d4
victor104 <sinensia104@VINET04>
2017-06-22 19:36:29 +0200

    Confirmación 1

---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_If.vb
@@ -0,0 +1,40 @@
+﻿Module Ejemplo_If
+    Sub Main()
+        Const OPERADOR As Char = "/"c
+        Dim operador1 As Single = 5
+        Dim operador2 As Single = 2
+        Dim resultado As Single
+        Dim operadorValido As Boolean = True
+
+        resultado = If(OPERADOR = "+",
+                operador1 + operador2,
+                If(OPERADOR = "*",
+                    operador1 * operador2,
+                    If(OPERADOR = "/",
+                        operador1 / operador2,
+                        If(OPERADOR = "-",
+                            operador1 - operador2,
+                            0)
+                    )))
+
+        If OPERADOR = "+" Then
+            resultado = operador1 + operador2
+        ElseIf OPERADOR = "*" Then
+            resultado = operador1 * operador2
+        ElseIf OPERADOR = "/" Then
+            resultado = If(operador2 = 0, 0, operador1 / operador2)
+            operadorValido = (operador2 <> 0)
+        ElseIf OPERADOR = "-" Then
+            resultado = operador1 - operador2
+        Else
+            operadorValido = False
+        End If
+
+        If Not operadorValido Then
+            Console.WriteLine("El operador no es valido")
+        Else
+            Console.WriteLine("El resultado de " & operador1 & " " & OPERADOR & " " & operador2 & " = " & resultado)
+        End If
+        System.Console.Read()
+    End Sub
+End Module
---
+++ App_1_Ejemplos_VB_Basicos/App_1_Ejemplos_VB_Basicos/Ejemplo_If_String.vb
@@ -0,0 +1,20 @@
+﻿Module Ejemplo_If_String
+
+    Sub Main()
+        Dim numClientes As Integer = 98
+        Dim etiqueta As Integer = If(numClientes <= 100,
+        "Menos de 100 clientes", "Al menos 100 clientes")
+        'Muestra "Menos de 100 clientes" 
+        Console.WriteLine(etiqueta)
+
+        numClientes = 120
+        etiqueta = If(numClientes <= 100, "Menos de 100 clientes",
+        "Al menos 100 clientes")
+
+        'Muestra "Al menos 100 clientes" 
+        Console.WriteLine(etiqueta)
+
+        System.Console.Read()
+    End Sub
+
+End Module
